045935f839cfd30146571628f3e29b04a30d6853
tspyrosk <30783136+tspyrosk@users.noreply.github.com>
2022-04-08 13:01:19 +0300

    Add extra conv to decoder

--- DeepSMOTE_MNIST.py
+++ DeepSMOTE_MNIST.py
@@ -111,6 +111,9 @@ class Decoder(nn.Module):
             nn.ConvTranspose2d(self.dim_h * 4, self.dim_h * 2, 4, 2, 1),
             nn.BatchNorm2d(self.dim_h * 2),
             nn.ReLU(True),
+            nn.ConvTranspose2d(self.dim_h * 2, self.dim_h, 4, 2, 1),
+            nn.BatchNorm2d(self.dim_h),
+            nn.ReLU(True),
             nn.ConvTranspose2d(self.dim_h * 2, 3, 4, 2, 1),
             #nn.Sigmoid())
             nn.Tanh())
